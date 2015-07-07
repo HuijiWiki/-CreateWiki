e1b26d6263d1b4ca4cbba707c7c07ded1a681072
Glen Gu <guxi99@gmail.com>
2015-07-07 11:58:07 +0800

    fix yige's bug

--- ErrorMessage.php
+++ ErrorMessage.php
@@ -7,7 +7,7 @@ class ErrorMessage{
 	const ERROR_DOMAIN_IS_EMPTY                        = 3;
 	const ERROR_DOMAIN_TOO_LONG                        = 4;
 	const ERROR_DOMAIN_TOO_SHORT	                   = 5;
-	const ERROR_DOMAIN_INVALID_CHAR                    = 17;
+
 
 	//create directory error messages
 	const ERROR_FAIL_FOLDER                            = 6;
@@ -21,23 +21,23 @@ class ErrorMessage{
 	//update database error message 
 	const ERROR_DATABASE_SCRIPT_ERROR                  = 10;
         
-        //User Not Login 
-        const ERROR_NOT_LOG_IN                             = 11;
-        
-        //Revoke errors
-        const ERROR_REMOVE_DIR                             = 12;
-        const ERROR_REVOKE_INSTALL                         = 13;
-        
-        //DB errors;
-        //to-do: add more db errors and modify the db class 
-        //using pdd. http://www.binpress.com/tutorial/using-php-with-mysql-the-right-way/17
-        const ERROR_DB_CONNECT                             = 14;
-        
-        
-        //INVITATION CODE ERROR
-        const INV_NOT_FOUND                                = 15;
-        const INV_USED                                     = 16;
-        
+    //User Not Login 
+    const ERROR_NOT_LOG_IN                             = 11;
+    
+    //Revoke errors
+    const ERROR_REMOVE_DIR                             = 12;
+    const ERROR_REVOKE_INSTALL                         = 13;
+    
+    //DB errors;
+    //to-do: add more db errors and modify the db class 
+    //using pdd. http://www.binpress.com/tutorial/using-php-with-mysql-the-right-way/17
+    const ERROR_DB_CONNECT                             = 14;
+    
+    
+    //INVITATION CODE ERROR
+    const INV_NOT_FOUND                                = 15;
+    const INV_USED                                     = 16;
+	const ERROR_DOMAIN_INVALID_CHAR                    = 17;
         
         
         
